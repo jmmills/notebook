1f1266331237be22e0c59d176a69419a7b17aa4b
Jason Mills <jason@yarp.me>
2013-11-02 21:14:12 -0700

    bubble_sort.pl

---
+++ bubble_sort.pl
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+use Data::Dumper;
+
+sub bubble_sort {
+	my @a = @_;
+	for my $i (0 .. $#a ) {
+		for my $j ( $i + 1 .. $#a ) {
+			$a[$j] < $a[$i] and @a[$i, $j] = @a[$j, $i]
+		}
+	}
+	return @a;
+}
+
+my @list = map {
+	int(rand(10));
+} (1..int(rand(10)));
+
+warn Dumper(\@list);
+warn "---";
+
+warn Dumper([bubble_sort(@list)]);
+
