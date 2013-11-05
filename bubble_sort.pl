5594053caf3ff5b80bf6bcf537647ff0ca3e2110
Jason Mills <jason@yarp.me>
2013-11-05 15:09:23 -0800

    add a not-so-perlish bubble sort

--- bubble_sort.pl
+++ bubble_sort.pl
@@ -12,12 +12,26 @@ sub bubble_sort {
 	return @a;
 }
 
+sub bubble_sort_2 {
+	for (my $a = 0; $a < $#_; $a++) {
+		for (my $b = 0; $b < $#_ ; $b++) {
+			if ($_[$b] < $_[$a]) {
+				@_[$a, $b] = @_[$b, $a]
+			}
+			
+		}
+	}
+}
+
 my @list = map {
 	int(rand(1000));
 } (1..int(rand(25)));
 
 warn Dumper(\@list);
 warn "---";
-
 warn Dumper([bubble_sort(@list)]);
 
+bubble_sort_2(@list);
+
+warn "---";
+warn Dumper([bubble_sort(@list)]);
