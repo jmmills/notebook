d0f527d1546adca1fb0e81d3c23a21f21a17419b
Jason Mills <jason@yarp.me>
2013-11-14 17:53:37 -0800

    2.pl

--- eurler/2.pl
+++ eurler/2.pl
@@ -1,8 +1,12 @@
 #!/usr/bin/env perl
 
 use 5.12.1;
-use Mouse;
 
-for ( my @seq = (1,2); $seq[2] = $seq[0] + $seq[1] <= 4_000_000; shift @seq ) {
-    say $seq[2] 
+my $sum = 2;
+
+for ( my @seq = (1,2); ($seq[2] = $seq[0] + $seq[1]) <= 4_000_000; shift(@seq) ) {
+    say $seq[2];
+    $sum += !($seq[2] % 2)? $seq[2] : 0;
 }
+
+say "Sum: $sum";
