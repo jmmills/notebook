ce1d0c7e156086e641061043ed58a238389c3439
Jason Mills <jmmills@cpan.org>
2016-03-06 16:08:54 -0800

    linear search

---
+++ python/cs/search/linear.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+def search(lst, value):
+  for idx, v in enumerate(lst):
+    if value == v:
+      return idx
+
+  return False
+
+class Test(TestCase):
+  a = []
+
+  def setUp(self):
+    self.a = [23,4,3,5,1,4,8]
+
+  def tearDown(self):
+    self.a = []
+
+  def test_search_true(self):
+    self.assertEqual(search(self.a, 4), 1)
+    self.assertEqual(search(self.a, 5), 3)
+
+  def test_search_false(self):
+    self.assertFalse(search(self.a, 100))
